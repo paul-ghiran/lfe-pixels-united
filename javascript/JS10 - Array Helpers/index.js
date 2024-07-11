02522cba02d22638ed2f71bb7d2d78a90c09e5ff
Paul Ghiran <webdev@paulghiran.com>
2024-07-11 22:00:31 +0300

    JS10 slice and splice

--- javascript/JS10 - Array Helpers/index.js
+++ javascript/JS10 - Array Helpers/index.js
@@ -98,6 +98,152 @@ console.log("EXEMPLU MAP");
 function calculeazaPatratul(nr) {
     return nr ** 2
 }
-console.log("patratele sunt ", arrayExemplu.map(calculeazaPatratul))
+const arrayPatrate = arrayExemplu.map(calculeazaPatratul)
+console.log("patratele sunt ", arrayPatrate)
 
 console.log("arrayExemplu este", arrayExemplu)
+
+// FILTER 
+// We filter the items from an array, based on a condition evaluated by a function
+console.warn("EXEMPLU FILTER");
+
+function verificaDacaNumarulEsteImpar(numar) {
+    return numar % 2 == 1;
+}
+const numereleImpare = arrayExemplu.filter(verificaDacaNumarulEsteImpar);
+
+console.log("Numerele impare sunt", numereleImpare);
+function filtreazaDoarNumereImpare(arrayDeFiltrat) {
+    const arrayRezultat = [];
+
+    for (var i = 0; i < arrayDeFiltrat.length; i++) {
+        if (arrayDeFiltrat[i] % 2 == 1) {
+            arrayRezultat.push(arrayDeFiltrat[i])
+        }
+    }
+
+    return arrayRezultat;
+}
+const numereleImpare2 = filtreazaDoarNumereImpare(arrayExemplu);
+
+console.log("Numerele impare2 sunt", numereleImpare2);
+
+// Includes 
+// Input: Un element care se cauta in array
+// Returneaza true / false daca elementul este gasit in array
+console.log("Inceput exemplu includes")
+console.log("arrayExemplu este", arrayExemplu)
+
+console.log("arrayExemplu include pe 3", arrayExemplu.includes(3));
+console.log("arrayExemplu include pe 6", arrayExemplu.includes(6));
+
+function existaElementul(arrayDeParcurs, elementDeCautat) {
+    for (i = 0; i < arrayDeParcurs.length; i++) {
+        if (arrayDeParcurs[i] == elementDeCautat) {
+            return true
+        }
+    }
+    return false;
+}
+console.log("arrayExemplu include pe 4", existaElementul(arrayExemplu, 4));
+console.log("arrayExemplu include pe 7", existaElementul(arrayExemplu, 7));
+
+// IndexOf 
+// Input: Un element care se cauta in array
+// Returneaza pozitia elementului sau -1 daca nu se gaseste
+console.log("Inceput exemplu indexOf")
+console.log("arrayExemplu este", arrayExemplu)
+
+console.log("arrayExemplu il include pe 3 pe pozitia", arrayExemplu.indexOf(3));
+console.log("arrayExemplu il include(?) pe 6 pe pozitia", arrayExemplu.indexOf(6));
+
+function gasestePozitiaElementului(arrayDeParcurs, elementDeCautat) {
+    for (i = 0; i < arrayDeParcurs.length; i++) {
+        if (arrayDeParcurs[i] == elementDeCautat) {
+            return i
+        }
+    }
+    return -1;
+}
+console.log("arrayExemplu il include(?) pe 4 pe pozitia", gasestePozitiaElementului(arrayExemplu, 4))
+console.log("arrayExemplu il include(?) pe 7 pe pozitia", gasestePozitiaElementului(arrayExemplu, 7))
+// Slice 
+// Input: Pozitia unui element de unde incepem taierea
+//        Numarul de elemente 
+// Returneaza un nou array mai mic, taiat din cel mare 
+console.log("INCEPUT EXEMPLU slice ")
+console.log("arrayexemplu era", arrayExemplu)
+const copieArray = arrayExemplu.slice(1, 3)
+console.log("copieArray arrayexemplu este", copieArray)
+
+// Cerinta: Se doreste copierea elementor dintr-un array pana la identificarea lui 12;
+const arrayEx1Slice = [1, 3, 6, 4, 12, 8, 9, 10, 150];
+const pozitiaLui12 = arrayEx1Slice.indexOf(12)
+
+const sliceUlPanaLa12 = arrayEx1Slice.slice(0, pozitiaLui12 + 1)
+console.log("Slice-ul pana la 12", sliceUlPanaLa12)
+// Cerinta: Se doreste copierea elementor dintr-un array de la identificarea lui 12;
+const sliceUlDupa12 = arrayEx1Slice.slice(pozitiaLui12 + 1, pozitiaLui12 + 4)
+console.log("Slice-ul dupa 12", sliceUlDupa12)
+
+function sliceArray(tortDeProcesat, startPosition, endPosition) {
+    const arrayRezultat = [];
+
+    for (var i = startPosition; i < endPosition; i++) {
+        const bucaticaCurentaDeTort = tortDeProcesat[i];
+
+        arrayRezultat.push(bucaticaCurentaDeTort);
+    }
+
+    return arrayRezultat;
+}
+console.log("Slice-ul dupa 12", sliceArray(arrayEx1Slice, pozitiaLui12 + 1, pozitiaLui12 + 4))
+
+// Splice 
+// Input: Pozitia unui element de unde incepem taierea
+//        Numarul de elemente 
+//        Ce adaugam in loc
+// Modifica array-ul 
+
+console.warn("EXEMPLU SPLICE")
+console.log("arrayEx1Slice este", arrayEx1Slice)
+
+// Use case1: Stergeti un element din array (sau mai multe, parametrul secund)
+const pozitiaLui6 = arrayEx1Slice.indexOf(6);
+
+const result = arrayEx1Slice.splice(pozitiaLui6, 1);
+console.log("arrayEx1Slice spliced", result)
+console.log("arrayEx1Slice fara 6 este", arrayEx1Slice)
+
+// Use case 2: Inlocuiti un element din array 
+// mai bine zis: Stergeti elemente din array, si adaugati elemente noi
+arrayEx1Slice.splice(1, 1, 100)
+console.log("arrayEx1Slice cu 3 inlocuit cu 100 este", arrayEx1Slice)
+
+// Use case 3: Adaugati un element sau mai multe in array, fara sa stergeti nimic 
+// explicatie: Ii dam sa stearga 0 elemente
+arrayEx1Slice.splice(1, 0, 200)
+console.log("arrayEx1Slice use case 3 este", arrayEx1Slice)
+
+function pseudoSPlice(arrayulDeProcesat, startPosition, deletedElements, addedElement) {
+    const arrayRezultat = [];
+
+    for (var i = 0; i < arrayulDeProcesat.length; i++) {
+        if (i >= startPosition && i < startPosition + deletedElements) {
+            // Suntem la elementele care trebuie sterse
+            console.log("SUNT PE POZITIA i, si nu introduc numere", i)
+            if (i == startPosition && addedElement !== undefined) {
+                arrayRezultat.push(addedElement)
+            }
+        } else {
+            arrayRezultat.push(arrayulDeProcesat[i])
+        }
+    }
+
+    return arrayRezultat;
+}
+
+console.log("Inceput test", arrayEx1Slice);
+console.log("rez", pseudoSPlice(arrayEx1Slice, 1, 4, 2))
+
+console.log("rez", pseudoSPlice(arrayEx1Slice, arrayEx1Slice.indexOf(9), 1))
