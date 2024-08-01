be79d94029f594f511d5988b6403ee5c6dbfd947
Paul Ghiran <webdev@paulghiran.com>
2024-08-01 21:28:38 +0300

    Ex2Alt

--- javascript/JS16 - Events + Mini recap/farmlife/script.js
+++ javascript/JS16 - Events + Mini recap/farmlife/script.js
@@ -1,28 +1,36 @@
 //1. Creati un obiect in care sa tinem minte cate vaci avem la ferma, si cate rate
 // 10 vaci, 5 rate
 
-// Calculati suma animalelor 
+// Calculati suma animalelor
 
 const ferma = {
-    vaci: 10,
-    rate: 5
+  vaci: 10,
+  rate: 5,
 };
 
 ferma.total = ferma.vaci + ferma.rate;
 
 console.log(ferma);
+console.log(ferma.total);
 
-function calculeazaPicioare(animal){
-    var numarPicioare=0;
-    if(animal==='vaci'){
-        numarPicioare=ferma.vaci*4;
-    }else if(animal ==='rate'){
-        numarPicioare=ferma.rate*2;
-    };
+// ex 2
+function calculeazaPicioare(animal) {
+  var numarPicioare = 0;
+  if (animal === "vaci") {
+    numarPicioare = ferma.vaci * 4;
+  } else if (animal === "rate") {
+    numarPicioare = ferma.rate * 2;
+  }
 
-    return numarPicioare;
+  return numarPicioare;
 }
 
-const picioareVaci=calculeazaPicioare('vaci')
-const picioareRate=calculeazaPicioare('rate')
-console.log(picioareVaci+picioareRate)
+const picioareVaci = calculeazaPicioare("vaci");
+const picioareRate = calculeazaPicioare("rate");
+console.log(picioareVaci + picioareRate);
+
+// alternativ ex 2
+function totalPentruFerma(fermaInput) {
+  return fermaInput.vaci * 4 + fermaInput.rate * 2;
+}
+console.log(totalPentruFerma(ferma));
