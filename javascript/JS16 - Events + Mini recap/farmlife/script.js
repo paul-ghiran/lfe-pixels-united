156ecdc4b5fb9c367367e167c8bf195f7d40de00
filipioanag12 <filip.ioanag12@gmail.com>
2024-08-01 21:13:41 +0300

    Suma animalelor
    Suma animalelor

---
+++ javascript/JS16 - Events + Mini recap/farmlife/script.js
@@ -0,0 +1,13 @@
+//1. Creati un obiect in care sa tinem minte cate vaci avem la ferma, si cate rate
+// 10 vaci, 5 rate
+
+// Calculati suma animalelor 
+
+const ferma = {
+    vaci: 10,
+    rate: 5
+};
+
+ferma.total = ferma.vaci + ferma.rate;
+
+console.log(ferma);
