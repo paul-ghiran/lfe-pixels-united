6449183e6293aab24d77f0306c82f8c072b88900
lioped <89127439+lioped@users.noreply.github.com>
2024-08-08 19:32:26 +0300

    ex

---
+++ javascript/JS18 EX/ex.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Se filtreaza un array, returnandu-se elementele divizibile cu 3
+
+//ex 1
+
+const arr = [1, 2, 4, 55, 5, 3, 6];
+const diviziblCu3 = arr.filter((word) => word % 3 === 0);
+console.log(diviziblCu3);
