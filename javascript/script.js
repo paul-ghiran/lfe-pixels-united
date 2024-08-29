05aedf7a195fafeffae37c2d9a6b05ff6ffe2307
lioped <89127439+lioped@users.noreply.github.com>
2024-08-29 21:52:52 +0300

    comm cu server

--- javascript/script.js
+++ javascript/script.js
@@ -26,24 +26,17 @@ const printeazaProdus = function (product) {
 
   elementProdus.innerHTML = `<li><img src="${product.image}" /><h2>${product.name}</h2></li>`;
 
-  const butonAdaugareProdus = document.createElement('button');
+  const butonAdaugareProdus = document.createElement("button");
   butonAdaugareProdus.innerText = "Adauga In Cos";
   elementProdus.appendChild(butonAdaugareProdus);
-
+  butonAdaugareProdus.addEventListener("click", function () {
+    addToCart(product);
+  });
 };
 
 const printeazaProduse = (arrayDeProduse) => arrayDeProduse.forEach((produs) => printeazaProdus(produs));
 
-printeazaProduse(listaProduse);
-
 const cartProduse = [];
-
-
-addToCart(listaProduse[0]);
-addToCart(listaProduse[0]);
-addToCart(listaProduse[0]);
-addToCart(listaProduse[1]);
-addToCart(listaProduse[1]);
 
 console.log(cartProduse);
 
@@ -60,19 +53,23 @@ console.log(calculateTotal(listaProduse));
 // };
 // console.log(calculateTotal2(listaProduse));
 
-
 function printeazaElementCart(product) {
   const cartHTML = document.querySelector("#cart");
   const elementCart = document.createElement("div");
   cartHTML.appendChild(elementCart);
   elementCart.innerHTML = `<h4>${product.name} (${product.price})</h4> <hr />`;
-};
+}
 
 function printeazaCart(cartIntreg) {
   const cartHTML = document.querySelector("#cart");
   cartHTML.innerHTML = "";
   cartIntreg.forEach(printeazaElementCart);
-  cartHTML.innerHTML += "Pret total:" + calculateTotal(cartIntreg)
+  cartHTML.innerHTML += "Pret total:" + calculateTotal(cartIntreg);
+  const buttonSubmit = document.createElement("button");
+  cartHTML.appendChild(buttonSubmit);
+  buttonSubmit.className = "clasa-button";
+  buttonSubmit.innerHTML = "Submit Order";
+  buttonSubmit.addEventListener("click", trimiteComanda);
 }
 
 function addToCart(produs) {
@@ -81,3 +78,25 @@ function addToCart(produs) {
 }
 
 printeazaCart(cartProduse);
+
+const fetchData = async function () {
+  const raspunsRequest = await fetch("https://paulghiran.com/messages/product.php");
+  const arrayProduse = await raspunsRequest.json();
+  console.log(arrayProduse);
+  printeazaProduse(arrayProduse);
+};
+fetchData();
+
+async function trimiteComanda() {
+  const continutBody = JSON.stringify({
+    order: cartProduse.map((produs) => produs.id),
+  });
+  const promise = fetch("https://paulghiran.com/messages/order.php", {
+    headers: {
+      "Content-Type": "application/json",
+      userkey: "london",
+    },
+    method: "POST",
+    body: continutBody,
+  });
+}
